7447565e5ecfb3ccf25dd4cf42c7c04100f1d6ac
Massimiliano Roccamena <massimiliano.roccamena@mail.polimi.it>
2020-05-03 12:14:36 +0200

    fix & update

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-var http = require("http")
-var url = require("url")
-var fs = require("fs")
+var express = require('express');
+var app = express();
 
-http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "./public" + q.pathname;
-    
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }  
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+app.use(express.static('public'));
+
+var server = app.listen(8080, function () {
+    console.log("Server running at http://localhost:" + 8080 + "/\nCTRL + C to shutdown");
+});
