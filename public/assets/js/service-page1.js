9331a60c30498aa7b3ebb2872ecb29563d5a244a
Massimiliano Roccamena <massimiliano.roccamena@mail.polimi.it>
2020-05-17 15:42:24 +0200

    fix

--- public/assets/js/service-page1.js
+++ public/assets/js/service-page1.js
@@ -1,12 +1,14 @@
-let h1 = document.querySelector("#service_name");
+var h1 = document.querySelector("#service_name");
 
-fetch("/service/0/page1").then(function(response){
-    if(!response.ok){
-        throw new Error("HTTP error, status =  " + response.status);
-    }
-    return response.json();
-})
-.then(function(json){
-    let data = json[0];
-    h1.innerHTML = '${data.name}';
-})
+$(document).ready(function () {
+    fetch("/api/service/0/page1").then(function(response){
+        if(!response.ok){
+            throw new Error("HTTP error, status =  " + response.status);
+        }
+        return response.json();
+    })
+    .then(function(json){
+        var data = json;
+        h1.innerHTML = '${data.name}';
+    });
+}
