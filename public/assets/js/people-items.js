8a3e68be22fece44a2da714660b993dc1912722b
Massimiliano Roccamena <massimiliano.roccamena@mail.polimi.it>
2020-05-17 22:37:53 +0200

    update

--- public/assets/js/people-items.js
+++ public/assets/js/people-items.js
@@ -3,18 +3,17 @@ $(document).ready(function () {
         if (!res.ok) { 
             throw new Error("HTTP error, status = " + res.status); 
         }
-        return res.json()
+        return res.json();
     }).then(function (json) {
+        var row = $("#people-row");
+
         for (var i=0; i<json.length; i++) {
             var data = json[i];
 
-            var row = $("#people-row");
             var col = $("<div class='col-sm-4'></div>");
 
-            col.appendTo(row);
+            row.append(col);
             col.load("/pages/templates/person-item.html");
-
-            $(".card-title").css( "background-color", "red" );
 
             var name = col.find(".card-title");
             name.text(data.name);
