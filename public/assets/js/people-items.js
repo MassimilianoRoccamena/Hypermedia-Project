577a8c08e9bb455341f210d6bd53d9e8a7d93aaf
Massimiliano Roccamena <massimiliano.roccamena@mail.polimi.it>
2020-05-17 23:43:54 +0200

    fix & update

--- public/assets/js/people-items.js
+++ public/assets/js/people-items.js
@@ -1,24 +1,43 @@
 $(document).ready(function () {
+    var row = $("#people-row");
+    var count = 15;
+    var page = 1;
+
+    //Create cols
+    for (let i=0; i<count; i++) {
+        let id = "person-col-" + i;
+        let col = $("<div id='" + id + "' class='col-sm-4'></div>");
+        row.append(col);
+    }
+    
+    //Load page 1
     fetch("/api/people/items?page=1").then(function (res) {
         if (!res.ok) { 
             throw new Error("HTTP error, status = " + res.status); 
         }
         return res.json();
     }).then(function (json) {
-        var row = $("#people-row");
-
         for (let i=0; i<json.length; i++) {
             let data = json[i];
 
-            let col = $("<div class='col-sm-4'></div>");
+            let id = "person-col-" + i;
+            let col = $("#"+id);
             col.load("/pages/templates/person-item.html", function(responseTxt, statusTxt, xhr) {
                 var name = col.find(".card-title");
                 name.text(data.name);
             });
-            
-            row.append(col);
         }
     })
+
+    //Pagination
+    $("#previous").click(function() {
+        if (page > 1) {
+
+        }
+    });
+    $("#next").click(function() {
+
+    });
 });
 
 
