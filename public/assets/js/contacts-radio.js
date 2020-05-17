f0f029a3ad41197907498d355f6210442b4a6e8f
Massimiliano Roccamena <massimiliano.roccamena@mail.polimi.it>
2020-05-18 00:21:11 +0200

    clean1

--- public/assets/js/contacts-radio.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () { 
-    $(".form-check-input").click(function() {
-        var label = $(this).next();
-        var info = label.children("#email");
-        $("#to").val(info.text());
-    });
-});
