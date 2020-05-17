c1d474bef8687d5ee79c50af1dc3516336bb727f
Massimiliano Roccamena <massimiliano.roccamena@mail.polimi.it>
2020-05-17 12:25:38 +0200

    update

---
+++ public/assets/js/people-items.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    //Create columns
+    row = $("#people-row");
+    for (var i=0; i<6; i++) {
+        id = "people-col" + i;
+        row.append("<div id=${id} class='col-sm-4'></div>");
+    }
+
+    //Load data
+    fetch("/people/items").then(function (res) {
+        if (!res.ok) { 
+            throw new Error("HTTP error, status = " + response.status); 
+        }
+        return response.json()
+    }).then(function (json) {
+        for (var i=0; i<json.length; i++) {
+            data = json[i];
+            col = $("#people-col"+i)
+            col.load("/pages/templates/person-item.html")
+        }
+    })
+});
+
