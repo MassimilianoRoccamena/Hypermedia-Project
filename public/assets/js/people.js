e80a1babcb5bc17111df61c7db0fc7d0a4a12a24
Massimiliano Roccamena <massimiliano.roccamena@mail.polimi.it>
2020-05-18 12:55:21 +0200

    update

--- public/assets/js/people.js
+++ public/assets/js/people.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+
+    //Orientation info
+    $('#orientation-info').load("/pages/components/orientation-info.html", function(responseTxt, statusTxt, xhr) {
+        addLink("Home", "/");
+        addLabel("People");
+    });
+
+    //Pagination
     $('#pagination').load("/pages/components/pagination.html", function(responseTxt, statusTxt, xhr) {
         $("#previous").click(function() {
             previousPage();
@@ -11,6 +19,23 @@ $(document).ready(function () {
     createCols(itemsCount);
     loadPage();
 });
+
+//------------------------------------- ORIENTATION INFO -----------------------------------------
+
+//Create orientation info label
+function addLabel(text) {
+    let li = $("<li class='breadcrumb-item'></li>")
+    li.text(text);
+    $("#orientation-ol").append(li);
+}
+
+//Create orientation info link
+function addLink(text, link) {
+    let li = $("<li class='breadcrumb-item'><a href='" + link + "'>"+ text +"</a></li>")
+    $("#orientation-ol").append(li);
+}
+
+//---------------------------------------- PAGINATION --------------------------------------------
 
 //Global variables
 var currentPage = 1,
