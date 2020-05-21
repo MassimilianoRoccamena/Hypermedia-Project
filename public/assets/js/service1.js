18a6653b9e124064283ce43bf043cf864f150700
Lorenzo Romanò <lorenzo1.romano@mail.polimi.it>
2020-05-21 13:24:14 +0200

    fix

--- public/assets/js/service1.js
+++ public/assets/js/service1.js
@@ -43,7 +43,7 @@ function loadData() {
     .then(function(json){
         let h1 = $("#name");
         let page2Link = $("#page2Link");
-        let href = $("<a href='/pages/service2.html/?id=" + id + "' class='btn btn-info' role='button'>Go to informations</a>");
+        let href = $("<a href='/pages/service2.html?id=" + id + "' class='btn btn-info' role='button'>Go to informations</a>");
         let p = $("#presentation_text");
         h1.append(json[0].name)
         page2Link.append(href);
