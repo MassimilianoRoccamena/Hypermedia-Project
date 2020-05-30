d06b914cd61490f8e8149806697159e1f2778306
Lorenzo Romanò <lorenzo1.romano@mail.polimi.it>
2020-05-30 14:41:17 +0200

    fix

--- public/assets/js/contacts.js
+++ public/assets/js/contacts.js
@@ -47,11 +47,11 @@ function sendHandler() {
                     if (body == "") {
                         alert("You must insert a body!");
                     } else {
-                        let message = {"name":name,"email":email,"title":title,"body":body}
+                        let message = {"sender_name":name,"sender_email":email,"title":title,"body":body};
                         
                         $.post("/api/message", message, function() {
                             alert("Message sent");
-                        })
+                        });
                     }
                 }
             }
